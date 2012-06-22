fb82c87eb4bbce828828579888b6ce568699b6d8
Conrad Irwin <conrad.irwin@gmail.com>
2012-06-22 01:58:53 -0700

    The simplest gist gem evar!

---
+++ jist.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name          = 'jist'
+  s.version       = '0.1'
+  s.summary       = 'Just allows you to upload gists'
+  s.description   = 'Provides a single function (Jist.gist) that uploads a gist.'
+  s.homepage      = 'https://github.com/ConradIrwin/jist'
+  s.email         = 'conrad.irwin@gmail.com'
+  s.authors       = ['Conrad Irwin']
+  s.files         = Dir["lib/**/*.rb"]
+  s.require_paths = ["lib"]
+
+  s.add_dependency 'multi_json'
+end
---
+++ lib/jist.rb
@@ -0,0 +1,55 @@
+require 'net/http'
+require 'multi_json'
+
+# It just gists.
+module Jist
+
+  module_function
+  # Upload a gist to https://gist.github.com/
+  #
+  # @param content  the code you'd like to gist
+  #
+  # @option :description  the description
+  # @option :filename  the filename (default 'a.rb')
+  # @option :public  to make it a public gist (default private)
+  # @option :username  if you wish to log in to github
+  # @option :password  (required if username is set)
+  #
+  # @return Hash  the decoded JSON response from the server
+  # @raise Exception  if something went wrong
+  def gist(content, options={})
+    json = {}
+
+    json[:description] = options[:description] if options[:description]
+    json[:public] = !!options[:public]
+
+    filename = options[:filename] || 'a.rb'
+
+    json[:files] = {
+      filename => {
+        :content => content
+      }
+    }
+
+    connection = Net::HTTP.new("api.github.com", 443)
+    connection.use_ssl = true
+    connection.read_timeout = 10
+
+    request = Net::HTTP::Post.new("/gists")
+    request.body = MultiJson.encode(json)
+
+    if options[:username]
+      request.basic_auth(options[:username], options[:password])
+    end
+
+    response = connection.start do |http|
+                 http.request(request)
+               end
+
+    if Net::HTTPCreated === response
+      MultiJson.decode(response.body)
+    else
+      raise RuntimeError.new "Got #{response.class} from gist: #{response.body}"
+    end
+  end
+end
