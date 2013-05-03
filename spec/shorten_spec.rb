63300c151084697a4f7cded16ecb82369044afc7
Conrad Irwin <conrad.irwin@gmail.com>
2013-05-02 19:33:48 -0700

    Fix specs when not logged in

--- spec/shorten_spec.rb
+++ spec/shorten_spec.rb
@@ -1,11 +1,11 @@
 describe '...' do
   before do
-    stub_request(:post, /api\.github.com\/gists\?access_token=.*/).to_return(:body => '{"html_url": "http://github.com/"}')
+    stub_request(:post, /api\.github.com\/gists/).to_return(:body => '{"html_url": "http://github.com/"}')
     stub_request(:post, "http://git.io/").to_return(:status => 201, :headers => { 'Location' => 'http://git.io/XXXXXX' })
   end
 
   it "should return a shortened version of the URL" do
-    Gist.gist("Test gist", :output => :short_url).should == "http://git.io/XXXXXX"
+    Gist.gist("Test gist", :output => :short_url, :anonymous => true).should == "http://git.io/XXXXXX"
   end
 end
 
