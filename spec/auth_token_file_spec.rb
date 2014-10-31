8ea0372521733bbd5b3d2260b9a7109dec8bb1a3
Jason Karns <jason@karns.name>
2014-10-31 14:10:08 -0400

    use a different ENV var for testing

--- spec/auth_token_file_spec.rb
+++ spec/auth_token_file_spec.rb
@@ -1,10 +1,11 @@
 require_relative '../lib/auth_token_file'
 
 describe Gist::AuthTokenFile do
-  before(:context) { Gist::URL_ENV_NAME = "GITHUB_URL" }
+  before(:each) do
+    stub_const("Gist::URL_ENV_NAME", "STUBBED_GITHUB_URL")
+  end
 
   context "without custom GITHUB_URL" do
-
     describe "#filename" do
       let(:filename) { File.expand_path "~/.gist" }
 
@@ -17,7 +18,9 @@ describe Gist::AuthTokenFile do
 
   context "with custom GITHUB_URL" do
     let(:github_url) { "gh.custom.org" }
-    before { ENV[Gist::URL_ENV_NAME] = github_url }
+    before do
+      ENV[Gist::URL_ENV_NAME] = github_url
+    end
 
     describe "#filename" do
       let(:filename) { File.expand_path "~/.gist" }
