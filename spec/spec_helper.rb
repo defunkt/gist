b5999d0a367f90e2789f247b133370ee0d51301d
Jason Karns <jason@karns.name>
2014-10-30 20:36:02 -0400

    enable :should syntax for rspec-mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,11 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-  config.expect_with :rspec do |c|
-    c.syntax = :should
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :should
+  end
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :should
   end
 end
 
