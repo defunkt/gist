244dbf730f09c35da7e488a185a1a12a41184b4b
Jason Karns <jason@karns.name>
2014-10-30 20:31:33 -0400

    explicitly enable :should syntax for expectations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,10 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
 end
 
 require 'webmock/rspec'
 require_relative '../lib/gist'
-
