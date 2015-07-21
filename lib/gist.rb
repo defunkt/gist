defunkt/gist

commit 687c15a54f69a2e6d9ccc73b8644ab11188f03dc

Fix performance of piping through head

lib/gist.rb
@@ -152,6 +152,7 @@ module Gist
   # otherwise list public gists for given username (optional argument)
   #
   # @param [String] user
+  # @deprecated
   #
   # see https://developer.github.com/v3/gists/#list-gists
   def list_gists(user = "")
@@ -187,14 +188,14 @@ module Gist
     if user == ""
       access_token = auth_token()
       if access_token.to_s != ''
-        url << "/gists?access_token=" << CGI.escape(access_token)
+        url << "/gists?per_page=100&taccess_token=" << CGI.escape(access_token)
         get_gist_pages(url)
       else
         raise Error, "Not authenticated. Use 'gist --login' to login or 'gist -l username' to view public gists."
       end
 
     else
-      url << "/users/#{user}/gists"
+      url << "/users/#{user}/gists?per_page=100"
       get_gist_pages(url)
     end
 
@@ -227,6 +228,7 @@ module Gist
       body.each do |gist|
         description = "#{gist['description'] || gist['files'].keys.join(" ")} #{gist['public'] ? '' : '(secret)'}"
         puts "#{gist['html_url']} #{description.tr("\n", " ")}\n"
+        $stdout.flush
       end
 
     else

commit dcb63aaac1df64631017bcb986b4f5832e3d481d

Fix typo introduced in 687c15a
"taccess_token" ==> "access_token".

The wrong query string is preventing authenticated listing, so `--list`
without argument is wrongly listing recent gists on the entire
gist.github.com (instead of listing gists of the authenticated user) in
v4.4.1.

lib/gist.rb
@@ -188,7 +188,7 @@ module Gist
     if user == ""
       access_token = auth_token()
       if access_token.to_s != ''
-        url << "/gists?per_page=100&taccess_token=" << CGI.escape(access_token)
+        url << "/gists?per_page=100&access_token=" << CGI.escape(access_token)
         get_gist_pages(url)
       else
         raise Error, "Not authenticated. Use 'gist --login' to login or 'gist -l username' to view public gists."